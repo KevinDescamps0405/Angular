a0b80a4788b9df4e12f28016335af7ad0283b7f7
kedescamps <@KdeFgh34000p>
2022-05-23 14:46:13 +0200

    KDE - eventComponent

---
+++ src/app/events/events-list/events-list.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.scss']
+})
+export class EventsListComponent implements OnInit {
+
+  event = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '23/05/2022',
+    time: '10:00 am',
+    price: 599.99,
+    imageUrl: '/assets/images/basic-shield.png',
+    location: {
+      address: '54 rue des tulipes',
+      city: 'Montpellier',
+      country: 'France'
+    }
+  };
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+}
